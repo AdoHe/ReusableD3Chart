99ee9d00d5fdc36b46ae4eda75351c050908710d
Tony <coolhzb@163.com>
2013-07-27 22:51:21 +0800

    initial commit

---
+++ JavaScript/pieChart.js
@@ -0,0 +1,62 @@
+function pieChart(){
+	var margin = {top: 10, right: 10, bottom: 10, left: 10},
+		width = 300,
+		height = 250,
+		radius = Math.min(width, height) / 2,
+		arc = d3.svg.arc()
+			.outerRadius(radius - 10)
+			.innerRadius(0)
+		pie = d3.layout.pie();
+
+	var color = d3.scale.ordinal()
+		.range(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
+
+	function chart(selection){
+		selection.each(function(data){
+			console.log(data);
+			
+			//Update the pie-data
+			pie.sort(null).value(yValue);
+
+			//Select the svg element, if it exits
+			var svg = d3.select(this).selectAll('svg').data([data]);
+
+			//Otherwise create a svg element
+			var gEnter = svg.enter().append('svg').append('g');
+
+			//Update the outer dimensions
+			svg.attr('width', width)
+				.attr('height', height);
+
+			//Update the inner dimensions
+			var g = svg.select('g')
+				.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+			//Update the arc
+			var gg = g.selectAll('.arc')
+					.data(pie(data))
+				.enter().append('g')
+					.attr('class', 'arc');
+			
+			gg.append('path')
+				.attr('d', arc)
+				.style('fill', function(d){return color(d.data.age); });
+		});
+	}
+	
+	chart.x = function(_){
+		if(!arguments.length) return xValue;
+		xValue = _;
+
+		return this;
+	}
+
+	chart.y = function(_){
+		if(!arguments.length) return yVaule;
+		yValue = _;
+
+		return this;
+	}
+
+	return chart;
+}
---
+++ Server/index.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var stream = require('stream');
+
+var server = http.createServer(function(req, res) {
+	var filePath = '.' + req.url;
+	if(filePath == './')
+		filePath = '../index.html';
+
+	var extname = path.extname(filePath);
+	var contentType = 'text/html';
+	
+	switch(extname){
+		case '.js':
+			contentType = 'text/javascript';
+			filePath = '.' + filePath;
+			break;
+		case '.css':
+			contentType = 'text/css';
+			filePath = '.' + filePath;
+			break;
+		case '.csv':
+			contentType = 'text/csv';
+			filePath = '.' + filePath;
+			break;
+	}
+
+	fs.exists(filePath, function(exits){
+		if(exits){
+			var readStream = fs.createReadStream(filePath);
+			
+			readStream.on('error', function(err){
+				res.writeHead(500);
+				res.end(String(err));
+			});
+			
+			res.writeHead(200, {'Content-Type': contentType});
+			readStream.pipe(res);
+		}else{
+			res.writeHead(404);
+			res.end();
+		}
+	});
+});
+
+server.listen(8080);
+console.log('The server has started at port: 8080');
