cdd8484412ebc2720e9f662241f2b50c67c1f443
Tony <coolhzb@163.com>
2015-09-27 12:40:15 +0800

    Update this line chart with mouse over......

--- JavaScript/lineChart.js
+++ JavaScript/lineChart.js
@@ -1,34 +1,43 @@
 function lineChart(){
-	var margin = {top:20, right: 10, bottom: 30, left: 50},
-		width = 800,
+	var margin = {top:20, right: 50, bottom: 30, left: 50},
+		width = 960,
 		height = 500,
 		xValue = function(d){return d[0]; },
 		yValue = function(d){return d[1]; };
 
 	var xScale = d3.time.scale(),
 		yScale = d3.scale.linear(),
+		parseDate = d3.time.format("%d-%b-%y").parse,
 		xAxis = d3.svg.axis().scale(xScale).orient('bottom'),
-		yAxis = d3.svg.axis().scale(yScale).orient('left');
+		yAxis = d3.svg.axis().scale(yScale).orient('left'),
+		bisectDate = d3.bisector(function(d) { return d.date; }).left,
+    formatValue = d3.format(",.2f"),
+    formatCurrency = function(d) { return "$" + formatValue(d); };
 
 	var line = d3.svg.line().x(X).y(Y);
 
 	function chart(selection){
 		selection.each(function(data){
-			
+
 			console.log(data);
-			
-			data = data.map(function(d, i){
-				return [xValue.call(data, d, i), yValue.call(data, d, i)];
-			});
+
+			data.forEach(function(d) {
+        d.date = parseDate(d.date);
+        d.close = +d.close;
+      });
+
+      data.sort(function(a, b) {
+        return a.date - b.date;
+      });
 
 			//Update the x-scale
-			xScale.domain(d3.extent(data, function(d){return d[0]; }))
+			xScale.domain([data[0].date, data[data.length - 1].date])
 				.range([0, width]);
 
 			//Update the y-scale
-			yScale.domain([0, d3.max(data, function(d){return d[1]; })])
+			yScale.domain(d3.extent(data, function(d) { return d.close; }))
 				.range([height, 0]);
-			
+
 			//Select the svg if it exits
 			var svg = d3.select(this).selectAll('svg').data([data]);
 
@@ -58,17 +67,46 @@ function lineChart(){
 			//Update the line
 			g.select('.line')
 				.attr('d', line);
+
+			var focus = gEnter.append('g')
+			      .attr('class', 'focus')
+			      .style('display', 'none');
+
+			focus.append('circle')
+			  .attr('r', 4.5);
+
+			focus.append('text')
+			  .attr('x', 9)
+			  .attr('dy', '.35em');
+
+			gEnter.append("rect")
+      .attr("class", "overlay")
+      .attr("width", width)
+      .attr("height", height)
+      .on("mouseover", function() { focus.style("display", null); })
+      .on("mouseout", function() { focus.style("display", "none"); })
+      .on("mousemove", mousemove);
+
+      function mousemove() {
+        var x0 = xScale.invert(d3.mouse(this)[0]),
+            i = bisectDate(data, x0, 1),
+            d0 = data[i - 1],
+            d1 = data[i],
+            d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+        focus.attr("transform", "translate(" + xScale(d.date) + "," + yScale(d.close) + ")");
+        focus.select("text").text(formatCurrency(d.close));
+      }
 		});
 	}
 
 	//The x-value accessor for line generator
 	function X(d){
-		return xScale(d[0]);
+		return xScale(d.date);
 	}
 
 	//The y-value accessor for line generator
 	function Y(d){
-		return yScale(d[1]);
+		return yScale(d.close);
 	}
 
 	chart.width = function(_){
